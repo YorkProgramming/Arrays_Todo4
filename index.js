494db6a203aed1fb95f76a1aad8d70de74aaba28
Matthew York <myorkprogramming@gmail.com>
2023-07-18 23:17:49 -0700

    Double Trouble

--- index.js
+++ index.js
@@ -113,3 +113,18 @@ After pushing the intermediate sum to the result array, we reset the sum back to
 
 /* Create a function that changes a given array to list each original element twice, 
 retaining original order. Convert [4,"Ulysses",42,false] to [4,4,"Ulysses","Ulysses",42,42,false,false]. */
+
+function doubleTrouble(arr) {
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i], arr[i]);
+    }
+
+    return result;
+}
+
+// Example
+const myArray = [4, "Ulysses", 42, false];
+const modifiedArray = doubleTrouble(myArray);
+console.log(modifiedArray); // [4, 4, "Ulysses", "Ulysses", 42, 42, false, false]
